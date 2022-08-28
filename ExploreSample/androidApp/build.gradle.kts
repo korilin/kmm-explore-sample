42086e7b1267e9ed2b505e3dba9649936f7387d9
korilin <korilin.dev@gmail.com>
2022-08-28 18:09:19 +0800

    add compose dependencies

--- ExploreSample/androidApp/build.gradle.kts
+++ ExploreSample/androidApp/build.gradle.kts
@@ -1,14 +1,18 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("kotlin-parcelize")
 }
 
+val composeVersion = findProperty("compose.version") as String
+val composeCompilerVersion = findProperty("compose.compiler") as String
+
 android {
-    compileSdk = 32
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
     defaultConfig {
-        applicationId = "com.korilin.kmm.explore.android"
-        minSdk = 21
-        targetSdk = 32
+        applicationId = findProperty("android.applicationId") as String
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+        targetSdk = (findProperty("android.targetSdk") as String).toInt()
         versionCode = 1
         versionName = "1.0"
     }
@@ -17,11 +21,30 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeCompilerVersion
+    }
 }
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    // android
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // compose
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("androidx.compose.foundation:foundation-layout:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.material:material-icons-core:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
 }
--- ExploreSample/androidApp/src/main/java/com/korilin/kmm/explore/android/MainActivity.kt
+++ ExploreSample/androidApp/src/main/java/com/korilin/kmm/explore/android/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.korilin.kmm.explore.Greeting
 import android.widget.TextView
+import androidx.activity.compose.setContent
 
 fun greet(): String {
     return Greeting().greeting()
@@ -12,6 +13,6 @@ fun greet(): String {
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {  }
     }
 }
