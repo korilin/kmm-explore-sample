9d916ed70a7f21117a40d57e70de9668da49dd4c
korilin <korilin.dev@gmail.com>
2022-08-29 02:46:22 +0800

    Update MessagePoster.kt

--- ExploreSample/shared/src/commonMain/kotlin/com/korilin/kmm/explore/datasource/network/MessagePoster.kt
+++ ExploreSample/shared/src/commonMain/kotlin/com/korilin/kmm/explore/datasource/network/MessagePoster.kt
@@ -2,6 +2,9 @@ package com.korilin.kmm.explore.datasource.network
 
 import com.korilin.kmm.explore.model.ImageMessage
 import com.korilin.kmm.explore.model.PostMessage
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.json.Json
 
 private const val POST_MSG_KEY_DEVICE = "device"
 private const val POST_MSG_KEY_MSG = "msg"
@@ -13,12 +16,22 @@ class MessagePoster(
     private val baseUrl = REMOTE_URL
 
     suspend fun postMsg(message: PostMessage): Result<ImageMessage> {
-        val url = REMOTE_URL.plus(POST_MSG_PATH)
-        return requester.post(
+        val url = baseUrl.plus(POST_MSG_PATH)
+        val result = requester.post(
             url, mapOf(
                 POST_MSG_KEY_DEVICE to message.device,
                 POST_MSG_KEY_MSG to message.msg
             )
         )
+        return decodeBody(result)
     }
+
+    private inline fun <reified T> decodeBody(result: Result<String>) = if (result.isSuccess) {
+        result.getOrNull()?.let {
+            val imageMessage = Json.decodeFromString<T>(it)
+            Result.success(imageMessage)
+        } ?: Result.failure(NullPointerException("message is null"))
+    } else Result.failure(
+        result.exceptionOrNull() ?: NullPointerException("Result value is null")
+    )
 }
